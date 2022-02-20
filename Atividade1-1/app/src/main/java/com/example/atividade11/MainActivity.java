0990c048e39f053dc4e0689c375f4b439ef303de
Lucas Melo <lucasmelouni@outlook.com>
2022-02-19 22:23:35 -0300

    Add Atividade 1-1

---
+++ Atividade1-1/app/src/main/java/com/example/atividade11/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.atividade11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button yourbutton = (Button) findViewById(R.id.button2);
+        EditText number_one = (EditText)findViewById(R.id.editTextNumber);
+        EditText number_two = (EditText) findViewById(R.id.editTextNumber2);
+        TextView result_text = (TextView)findViewById(R.id.results);
+
+
+        yourbutton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        String field_1 = number_one.getText().toString();
+                        String field_2 = number_two.getText().toString();
+
+                        float result = Float.parseFloat(field_1) + Float.parseFloat(field_2);
+
+                        result_text.setText(String.format("Resultado: %s", result));
+
+                        //result_text.setText(field_1);
+
+
+
+                    }
+
+                });
+
+    }
+
+}
